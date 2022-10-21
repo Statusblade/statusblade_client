2a0c9f0e2d30c8e5efacad7d257be1199e70d6c4
PalermoD <palermo.deschamps@gmail.com>
2022-10-21 12:06:19 -0400

    Basic navigaion component

--- src/views/Profile/index.js
+++ src/views/Profile/index.js
@@ -1,14 +1,16 @@
-import React from "react"
+import React from "react";
 import { Container } from "../../components/container";
+import Navbar from "../../components/navBar";
 
 const Profile = () => {
   return (
     <React.Fragment>
       <Container>
+        <Navbar />
         Profile Page
       </Container>
     </React.Fragment>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
